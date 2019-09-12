5d9d848e7ceb61838e077accb4177d71b8c02c08
cclancy97 <cclancy@bhcc.edu>
2019-09-11 23:06:18 -0400

    first commit

---
+++ assets/scripts/config.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let apiUrl
+const apiUrls = {
+  production: 'https://secure-wave-76146.herokuapp.com',
+  development: 'http://localhost:4741'
+}
+
+if (window.location.hostname === 'localhost') {
+  apiUrl = apiUrls.development
+} else {
+  apiUrl = apiUrls.production
+}
+
+module.exports = {
+  apiUrl
+}
